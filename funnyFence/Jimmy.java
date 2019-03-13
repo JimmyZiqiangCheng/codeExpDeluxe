9eb81cd60d4cd74bd97faefac875bf0a1151db05
jimmycheng <chengziqiang@gmail.com>
2019-03-13 19:30:50 +1030

    add rest of the files

--- funnyFence/Jimmy.java
+++ funnyFence/Jimmy.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 class Jimmy{
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println("please enter your fence.");
+		String testString = input.nextLine();
+		System.out.println(getLength(testString));
 	}
 
-	public static int getLength(string s){
-		int currentLength = 0
-		char[] array = s.toCharArray();
+	public static int getLength(String s){
+		int currentLength = 1;
 		boolean isFunnyFence = false;
-		for (int start=0; start<array.length; start++){
-			for (int end=start; end<array.length; end++){
-				isFunnyFence=isFunny(array, start, end);
+		if (s.length() == 0){
+			return 0;
+		}
+		for (int start=0; start<s.length()-1; start++){
+			for (int end=start+1; end<s.length(); end++){
+				isFunnyFence=isFunny(s, start, end);
 				if (isFunnyFence){
+					int next = end-start+1;
 					currentLength = (int)Math.max(currentLength,next);
 				}
 			}
@@ -18,7 +26,14 @@ class Jimmy{
 		return currentLength;
 	}
 
-	public static boolean isFunny(char[] array, int start, int end){
-		
+	public static boolean isFunny(String s, int start, int end){
+		boolean isFunnyFence = true;
+		// check if the element next is different
+		for (int i=start; i<end; i++){
+			if (s.charAt(i)==s.charAt(i+1)){
+				isFunnyFence = false;
+			}
+		}
+		return isFunnyFence;
 	}
 }
