3b1cb1d26325ac73f2b150de4d7c4464fc6c89bc
Sam Karem <samkarem@Sams-MacBook-Pro.local>
2019-03-18 22:35:19 +1030

    basic done

--- Friendly Sequences/FriendlySequences.java
+++ Friendly Sequences/FriendlySequences.java
@@ -1,29 +1,49 @@
+import java.util.Scanner;
 import java.util.*;
 class FriendlySequences {
 
 	public static void main(String[] args) {
-		int[] array = { 1,2,3,4};
-		count(array);
+	    System.out.println("Please enter the numbers");
+        Integer[] array =  getInput();
+        System.out.println(count(array));
 	}
-	public static int count (int [] array) {
-
-		hashset(4554);
+    public static Integer[] getInput(){
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+        String[] numberString = string.split(" ");
+        Integer[] numbers = new Integer[numberString.length];
+        for(int i = 0; i < numberString.length; i++) {
+            numbers[i] = Integer.parseInt(numberString[i]);
+        }
+        return numbers;
+    }
+	public static int count (Integer [] array) {
+        int count = 0;
+        ArrayList<HashSet<Integer>> rows = new ArrayList<HashSet<Integer>>();
 		for (int i = 0; i < array.length; i++) {
-
+            rows.add(hashset(array[i]));
 		}
-		return 0;
+        for (int i=0; i < rows.size(); i++) {
+            for (int j=1; j < rows.size(); j++) {
+                if((rows.get(i)).equals(rows.get(j))) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+		return count;
 	}
 
-	public static void hashset( int element) {
+	public static HashSet<Integer> hashset( int element) {
 		char [] digits = String.valueOf(element).toCharArray();
 		Integer[] intDigits = new Integer[digits.length];
 		for(int i=0; i<digits.length; i++){
    			intDigits[i] = digits[i]-48;
 		}
-		Set<Integer> set = new HashSet<Integer>(Arrays.asList(intDigits));
+		HashSet<Integer> set = new HashSet<Integer>(Arrays.asList(intDigits));
 		System.out.println(intDigits);
 		System.out.println(set);
-        System.out.println("YASSS");
-
+        return set;
 	}
 }
